536d9b0c35735cd3dc6db7d687cef732b1e87dda
YEAHx4 <developerstarlight@gmail.com>
2023-11-26 22:13:47 +0900

    Init Spring boot for backend

---
+++ portpolent-backend/src/main/java/io/yeahx4/portpolent/PortpolentBackendApplication.java
@@ -0,0 +1,13 @@
+package io.yeahx4.portpolent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortpolentBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortpolentBackendApplication.class, args);
+    }
+
+}
---
+++ portpolent-backend/src/test/java/io/yeahx4/portpolent/PortpolentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.yeahx4.portpolent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortpolentBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
