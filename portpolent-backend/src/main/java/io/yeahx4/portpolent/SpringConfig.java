143138e79b1c81d05c69a0ab904c00611a422d7b
YEAHx4 <developerstarlight@gmail.com>
2023-11-28 03:12:57 +0900

    user entity and time base entity

--- portpolent-backend/src/main/java/io/yeahx4/portpolent/SpringConfig.java
+++ portpolent-backend/src/main/java/io/yeahx4/portpolent/SpringConfig.java
@@ -1,7 +1,13 @@
 package io.yeahx4.portpolent;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class SpringConfig {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
---
+++ portpolent-backend/src/main/java/io/yeahx4/portpolent/entity/TimeEntity.java
@@ -0,0 +1,34 @@
+package io.yeahx4.portpolent.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+public class TimeEntity {
+    @Column(nullable = false, length = 20)
+    @CreatedDate
+    private String createdAt;
+
+    @Column(nullable = false, length = 20)
+    @LastModifiedDate
+    private String updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"));
+        updatedAt = createdAt;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"));
+    }
+}
