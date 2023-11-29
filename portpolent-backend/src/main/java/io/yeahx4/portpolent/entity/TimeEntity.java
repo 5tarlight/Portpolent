132c75feb28d4812f35997b6e4623a6315e346b6
YEAHx4 <developerstarlight@gmail.com>
2023-11-29 15:35:13 +0900

    unzip whildcard imports

--- portpolent-backend/src/main/java/io/yeahx4/portpolent/entity/TimeEntity.java
+++ portpolent-backend/src/main/java/io/yeahx4/portpolent/entity/TimeEntity.java
@@ -1,6 +1,10 @@
 package io.yeahx4.portpolent.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
