3f78e63a4b8a74f85cbf5e4a6258e8c1c7199cd5
YEAHx4 <developerstarlight@gmail.com>
2023-11-29 15:50:59 +0900

    User account type

--- portpolent-backend/src/main/java/io/yeahx4/portpolent/entity/User.java
+++ portpolent-backend/src/main/java/io/yeahx4/portpolent/entity/User.java
@@ -1,7 +1,10 @@
 package io.yeahx4.portpolent.entity;
 
+import io.yeahx4.portpolent.entity.consts.AccountType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -28,4 +31,8 @@ public class User extends TimeEntity {
 
     @Column(nullable = false, length = 255)
     private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private AccountType accountType;
 }
