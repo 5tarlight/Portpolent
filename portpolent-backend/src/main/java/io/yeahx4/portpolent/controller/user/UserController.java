62d36228abad5488a9ce7c09f9b690bedd385f6f
YEAHx4 <developerstarlight@gmail.com>
2023-11-30 19:21:26 +0900

    save user service and encryption system

--- portpolent-backend/src/main/java/io/yeahx4/portpolent/controller/user/UserController.java
+++ portpolent-backend/src/main/java/io/yeahx4/portpolent/controller/user/UserController.java
@@ -1,10 +1,14 @@
 package io.yeahx4.portpolent.controller.user;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @PostMapping("/signup")
+    public void signUp() {
 
+    }
 }
--- portpolent-backend/src/main/java/io/yeahx4/portpolent/service/user/UserService.java
+++ portpolent-backend/src/main/java/io/yeahx4/portpolent/service/user/UserService.java
@@ -4,6 +4,7 @@ import io.yeahx4.portpolent.entity.User;
 import io.yeahx4.portpolent.entity.consts.AccountType;
 import io.yeahx4.portpolent.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,10 +12,12 @@ import java.util.Optional;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder encoder;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder encoder) {
         this.userRepository = userRepository;
+        this.encoder = encoder;
     }
 
     public boolean availableToCreate(String handle, String email) {
@@ -24,9 +27,17 @@ public class UserService {
         return emailSearch.isEmpty() && handleSearch.isEmpty();
     }
 
+    private String encryptPassword(String password) {
+        return this.encoder.encode(password);
+    }
+
+    public boolean doPasswordMatch(String original, String encrypted) {
+        return this.encoder.matches(original, encrypted);
+    }
+
     public User saveUser(String email, String handle, String username, String password, AccountType type) {
-        // TODO : Encrypt password!
-        User user = new User(-1, email, handle, username, password, type);
+        String encrypted = this.encryptPassword(password);
+        User user = new User(-1, email, handle, username, encrypted, type);
         return this.userRepository.save(user);
     }
 }
