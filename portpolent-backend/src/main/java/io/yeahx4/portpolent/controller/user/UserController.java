a7641227a60d73c2b36eaf4c58271a2c9680dcf7
YEAHx4 <developerstarlight@gmail.com>
2023-11-30 23:40:49 +0900

    Sign up dto

--- portpolent-backend/src/main/java/io/yeahx4/portpolent/controller/user/UserController.java
+++ portpolent-backend/src/main/java/io/yeahx4/portpolent/controller/user/UserController.java
@@ -1,14 +1,28 @@
 package io.yeahx4.portpolent.controller.user;
 
+import io.yeahx4.portpolent.dto.user.SignUpDto;
+import io.yeahx4.portpolent.service.user.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    @PostMapping("/signup")
-    public void signUp() {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final UserService userService;
 
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/signup")
+    public void signUp(@RequestBody SignUpDto dto) {
+        boolean availableToCreate = this.userService.availableToCreate(dto.handle(), dto.email());
     }
 }
