a78b414b64fd89742273d3cd40fd67c6739bb446
YEAHx4 <developerstarlight@gmail.com>
2023-12-01 23:31:06 +0900

    Sign up functionality

--- portpolent-backend/src/main/java/io/yeahx4/portpolent/controller/user/UserController.java
+++ portpolent-backend/src/main/java/io/yeahx4/portpolent/controller/user/UserController.java
@@ -1,10 +1,15 @@
 package io.yeahx4.portpolent.controller.user;
 
 import io.yeahx4.portpolent.dto.user.SignUpDto;
+import io.yeahx4.portpolent.entity.User;
+import io.yeahx4.portpolent.entity.consts.AccountType;
 import io.yeahx4.portpolent.service.user.UserService;
+import io.yeahx4.portpolent.util.RestResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,8 +27,27 @@ public class UserController {
     }
 
     @PostMapping("/signup")
-    public void signUp(@RequestBody SignUpDto dto) {
+    public ResponseEntity<RestResponse<User>> signUp(@RequestBody SignUpDto dto) {
         boolean availableToCreate = this.userService.availableToCreate(dto.handle(), dto.email());
 
+        if (!availableToCreate) {
+            return new ResponseEntity<>(
+                    RestResponse.fail("Duplicated email or handle."),
+                    HttpStatus.BAD_REQUEST
+            );
+        }
+
+        User user = this.userService.saveUser(
+                dto.email(),
+                dto.handle(),
+                dto.username(),
+                dto.password(),
+                AccountType.OWN
+        );
+
+        return new ResponseEntity<>(
+                RestResponse.success(user),
+                HttpStatus.OK
+        );
     }
 }
