268fd06619a61e6ec1ece6932ce7f56e40e1adfe
YEAHx4 <developerstarlight@gmail.com>
2023-12-03 21:19:12 +0900

    sign up dto annotational validation

--- portpolent-backend/src/main/java/io/yeahx4/portpolent/dto/user/SignUpDto.java
+++ portpolent-backend/src/main/java/io/yeahx4/portpolent/dto/user/SignUpDto.java
@@ -1,9 +1,24 @@
 package io.yeahx4.portpolent.dto.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public record SignUpDto(
+        @Email
+        @Size(max = 100)
+        @NotNull
         String email,
+
+        @Size(min = 4, max = 20)
+        @NotNull
         String handle,
+
+        @Size(min = 2, max = 10)
+        @NotNull
         String username,
+
+        @NotNull
         String password
 ) {
 }
