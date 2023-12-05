ef5d5bce61069faa3f07346b797f4698b3bea30e
YEAHx4 <developerstarlight@gmail.com>
2023-12-05 23:27:42 +0900

    Validation for dto fields via Regular Expressions

--- portpolent-backend/src/main/java/io/yeahx4/portpolent/dto/user/SignUpDto.java
+++ portpolent-backend/src/main/java/io/yeahx4/portpolent/dto/user/SignUpDto.java
@@ -2,6 +2,7 @@ package io.yeahx4.portpolent.dto.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record SignUpDto(
@@ -11,10 +12,12 @@ public record SignUpDto(
         String email,
 
         @Size(min = 4, max = 20)
+        @Pattern(regexp = "[a-z0-9]{4,20}")
         @NotNull
         String handle,
 
         @Size(min = 2, max = 10)
+        @Pattern(regexp = "[\\w가-힣]{2,10}")
         @NotNull
         String username,
 
