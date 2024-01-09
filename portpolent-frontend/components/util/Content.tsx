dee6de2f940ef1b081b3d7a86ea5dc057b7a5bec
YEAHx4 <developerstarlight@gmail.com>
2024-01-09 13:27:21 +0900

    Add custom classname prop to content container component

--- portpolent-frontend/components/util/Content.tsx
+++ portpolent-frontend/components/util/Content.tsx
@@ -2,10 +2,14 @@ import { ReactElement } from "react";
 
 export default function Content({
   children,
+  className = "",
 }: {
   children: ReactElement | number | string;
+  className?: string;
 }) {
   return (
-    <div className="max-w-[1280px] w-full justify-self-center">{children}</div>
+    <div className={"max-w-[1280px] w-full justify-self-center " + className}>
+      {children}
+    </div>
   );
 }
