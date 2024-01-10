8413ff14e759caa4a64cd480f9efbb5afe0db377
YEAHx4 <developerstarlight@gmail.com>
2024-01-11 00:52:52 +0900

    Replace "search" text in header search-box to svg icon

--- portpolent-frontend/components/header/Search.tsx
+++ portpolent-frontend/components/header/Search.tsx
@@ -1,3 +1,5 @@
+import { FaSearch } from "react-icons/fa";
+
 export default function Search() {
   return (
     <div className="lg:w-96 h-9 md:w-52 sm:flex hidden">
@@ -5,7 +7,9 @@ export default function Search() {
         placeholder="다른 포트폴리오 찾기"
         className="w-full pl-3 bg-gray-100 outline-none rounded-l-lg"
       />
-      <button className="w-16 bg-gray-400 rounded-r-lg">검색</button>
+      <button className="w-10 bg-gray-400 rounded-r-lg flex justify-center items-center">
+        <FaSearch />
+      </button>
     </div>
   );
 }
