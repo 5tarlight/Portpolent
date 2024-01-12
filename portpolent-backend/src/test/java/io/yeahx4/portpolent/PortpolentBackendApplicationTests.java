a502823b876a0379ff9ba8803f77959a7c5a5ec8
YEAHx4 <developerstarlight@gmail.com>
2024-01-12 20:16:52 +0900

    Test test

--- portpolent-backend/src/test/java/io/yeahx4/portpolent/PortpolentBackendApplicationTests.java
+++ portpolent-backend/src/test/java/io/yeahx4/portpolent/PortpolentBackendApplicationTests.java
@@ -1,13 +1,16 @@
 package io.yeahx4.portpolent;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PortpolentBackendApplicationTests {
-
     @Test
-    void contextLoads() {
+    void testShouldWork() {
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        Assertions.assertEquals(c, 3);
     }
-
 }
