fca2e5583b6c502e5129a07e4c2d26834a089976
YEAHx4 <developerstarlight@gmail.com>
2024-02-25 06:08:43 +0900

    Redirect to my page

--- portpolent-frontend/app/me/page.tsx
+++ portpolent-frontend/app/me/page.tsx
@@ -1,5 +1,26 @@
+"use client";
+
+import { encodeUrl } from "@/lib/backend";
+import { whoAmI } from "@/lib/user";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 const MyPage = () => {
-  return <div>My Page</div>;
+  const { push } = useRouter();
+
+  useEffect(() => {
+    (async () => {
+      const res = await whoAmI(false);
+
+      if (res.ok) {
+        push(encodeUrl("/profile", { handle: res.data.data.handle }));
+      } else {
+        push(encodeUrl("/signin", { t: "/me" }));
+      }
+    })();
+  });
+
+  return <h1 className="m-4 text-2xl">Redirecting... Please wait.</h1>;
 };
 
 export default MyPage;
